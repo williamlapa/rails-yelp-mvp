d4bfabd542bd48bb4ae5fb940b209c497bd6a581
william lapa <williamlapa@hotmail.com>
2020-11-27 17:08:31 -0300

    backend ok falta STYLE

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # get '/restaurants/top', to: 'restaurants#top' as: :top_st
+
+    # /restaurants/top - LINHAS SERVEM PARA LISTAR OS MELHORES rankings
+    collection do
+      # get '/top', to: 'restaurants#top'
+      get :top
+    end
+
+    # SERVE PARA CATEGORIZAR POR CHEF
+    member do
+      get :category
+    end
+
+    # LINHA ABAIXO MOSTRA QUE QUE SÃO NESTED ROUTES
+    resources :reviews, only: %i[new create]
+  end
+
+  resources :reviews, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning the db...'
+
+Restaurant.destroy_all
+
+puts 'db is clean'
+
+puts 'creating restaurants...'
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+
+  puts "#{restaurant.id}-#{restaurant.name} was created"
+end
